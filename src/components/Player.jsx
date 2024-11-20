b6e1668f8b7362db4fab115cb83669fc56248897
Sandeep Biswas <sandeep12.biswas@gmail.com>
2024-11-20 15:28:09 +0530

    new component Player is created

---
+++ src/components/Player.jsx
@@ -0,0 +1,11 @@
+export default function Player() {
+    return (
+        <li>
+            <span className='player'>
+                <span className='player-name'>Player 1</span>
+                <span className='player-symbol'>X</span>
+            </span>
+            <button>Edit</button>
+        </li>
+    );
+}
